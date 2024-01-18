91fac2cc7fbf992cdab797f91ddef6f1a261e683
rishnuvk <rishnuvk15@gmail.com>
2024-01-18 10:19:10 +0530

    emsback

---
+++ DB.connection/connection.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose')
+
+const connectionString=process.env.DATABASE
+
+
+// using mongoose connect
+
+mongoose.connect(connectionString,{
+    useUnifiedTopology:true,
+    useNewUrlParser:true
+
+}).then((data)=>{
+    console.log("mongo db atlas connected to ems server");
+}).catch((err)=>{
+    console.log('mongo db connection failed');
+})
+
